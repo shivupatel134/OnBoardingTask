f557e842761b4a3a5d8f63a348400ec7ba95f35a
Shivani Patel <Shivani Patel@DESKTOP-B130C8V>
2020-06-01 23:25:03 +1200

    Updates

---
+++ precache-manifest.1af00aaa32e4612e8a26c48d17a0efba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6c3b531d59001f62267992a8782dd4d",
+    "url": "/OnBoardingTask/index.html"
+  },
+  {
+    "revision": "998e643cee2bfcf2f4a1",
+    "url": "/OnBoardingTask/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1560388757219e7be8f2",
+    "url": "/OnBoardingTask/static/js/2.d1f6a5d8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/OnBoardingTask/static/js/2.d1f6a5d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998e643cee2bfcf2f4a1",
+    "url": "/OnBoardingTask/static/js/main.e9899f6b.chunk.js"
+  },
+  {
+    "revision": "bf71f9d4b798580fd59e",
+    "url": "/OnBoardingTask/static/js/runtime-main.599f04d4.js"
+  }
+]);
