95baf19e174f1544086e950ca084dad8ecdc9090
Shivani Patel <Shivani Patel@DESKTOP-B130C8V>
2020-06-16 18:45:48 +1200

    Updates

---
+++ precache-manifest.8b3f6f83bcaea7807025eb28144b33db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94a2cd957fb938f67e35ec6710b2a350",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d30cf4405765ca2205",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1e6dabca9414814cb04c",
+    "url": "/static/js/2.d57cc0a6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d57cc0a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d30cf4405765ca2205",
+    "url": "/static/js/main.c014da35.chunk.js"
+  },
+  {
+    "revision": "9e1bfe6b0d90b192fd76",
+    "url": "/static/js/runtime-main.60281a99.js"
+  }
+]);
